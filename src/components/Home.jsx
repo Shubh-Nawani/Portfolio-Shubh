8abc3781bfac61f7e170c8a859dcfed5ff79589d
Shubh-Nawani <shubhnawani@outlook.com>
2024-12-09 01:21:54 +0530

    final

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,9 +4,13 @@ import Shubh from '../assets/Shubh.jpg'
 const Home = () => {
   return (
     <>
-    <div className='home'>
-    <h1>Hi, I&apos;m Shubh Nawani, a full-stack developer...</h1>
-    <img src={Shubh} alt="shubh.jpg" />
+    <div className='home'> 
+    <div className="home-content">
+      <h1>Hi, I&apos;m Shubh Nawani, a full-stack developer...</h1>
+      <img src={Shubh} alt="shubh.jpg" />
+    </div>
+
+    
     </div>
     </>
   )
